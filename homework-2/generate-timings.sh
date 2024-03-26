5cb5b47d707034dada4310b6123375ee354228d4
AntonioCrivello <amc50@rice.edu>
2024-03-25 21:03:03 -0500

    Updated makefile

--- homework-2/generate-timings.sh
+++ homework-2/generate-timings.sh
@@ -9,11 +9,15 @@ save_checkpoint() {
     echo "$1 $2" > checkpoint.txt
 }
 
+# Flag indicating if the script has resumed from a checkpoint
+resumed_from_checkpoint=false
+
 # Function to resume from the last checkpoint
 resume_from_checkpoint() {
     if [ -f "checkpoint.txt" ]; then
         read -r i k < checkpoint.txt
         echo "Resuming from checkpoint: i=$i, k=$k"
+        resumed_from_checkpoint=true
     else
         i=4
         k=0
@@ -32,8 +36,21 @@ perform_work() {
     # Extract the timings for each matrix-matrix multiplication type
     local matmul_parallel=$(echo "$output" | tail -n 2 | head -n 1 | awk '{print $NF}')
     local matmul_parallel_collapsed=$(echo "$output" | tail -n 1 | awk '{print $NF}')
-    # recursive_timing=$(echo "$output" | tail -n 2 | head -n 1 | awk '{print $NF}')
-    # recursive_intermediate_timing=$(echo "$output" | tail -n 1 | awk '{print $NF}')
+
+    # Check for duplication only if resumed from checkpoint
+    if [ "$resumed_from_checkpoint" = true ]; then
+        local last_line=$(tail -n 1 "docs/timing.csv" 2>/dev/null)
+        local last_matrix_size=$(echo $last_line | cut -d ',' -f1)
+        local last_thread_number=$(echo $last_line | cut -d ',' -f2)
+        
+        # If the last entry matches the current, do not duplicate
+        if [[ "$last_matrix_size" -eq "$matrix_size" && "$last_thread_number" -eq "$thread_numbers" ]]; then
+            return
+        fi
+        
+        # Reset flag after the first check
+        resumed_from_checkpoint=false
+    fi
 
     # Output the timings and sizing to .csv for plotting
     echo "$matrix_size, $thread_numbers, $matmul_parallel, $matmul_parallel_collapsed" >> "docs/timing.csv"
@@ -47,13 +64,12 @@ main() {
     resume_from_checkpoint
     # Loop for matrix sizes of 2^i for i = 4, 5, 6,...,10
     for ((; i <= 10; i++)); do
-        # Loop for thread numbers of 2^k for k = 1,...,5
+        # Loop for thread numbers of 2^k for k = 0,...,5
         for ((; k <= 5; k++)); do
             perform_work
             save_checkpoint $i $k
-
         done
-        k=0 # Reset k to 1 for the next loop of i
+        k=0 # Reset k to 0 for the next loop of i
     done
     # Remove the checkpoint file after completion
     rm -f checkpoint.txt
@@ -61,5 +77,3 @@ main() {
 
 # Execute the main function
 main
-
-
