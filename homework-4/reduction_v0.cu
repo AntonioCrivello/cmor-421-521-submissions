5f066d32b6e0343f008414e6dac369baf6f9832b
AntonioCrivello <amc50@rice.edu>
2024-04-20 08:21:23 -0500

    Fixed formatting except for weird if 1

--- homework-4/reduction_v0.cu
+++ homework-4/reduction_v0.cu
@@ -4,44 +4,51 @@
 
 #define BLOCK_SIZE 128
 
-__global__ void partial_reduction(const int N, float *x_reduced, const float *x){
-  
+__global__ void partial_reduction(const int N, float *x_reduced, const float *x)
+{
+
   __shared__ float s_x[BLOCK_SIZE];
 
   const int i = blockDim.x * blockIdx.x + threadIdx.x;
   const int tid = threadIdx.x;
-  
+
   // Coalesced reads in
   s_x[tid] = 0.f;
-  if (i < N){
+  if (i < N)
+  {
     s_x[tid] = x[i];
   }
 
   // Number of "live" threads per block
   int alive = blockDim.x;
-  
+
   // Define initial stride size
   int s = 1;
-  while (alive > 1){
-    __syncthreads(); 
-    // Update the number of live threads    
+  while (alive > 1)
+  {
+    __syncthreads();
+    // Update the number of live threads
     alive /= 2;
-    if (tid % (2 * s) == 0){
+    if (tid % (2 * s) == 0)
+    {
       s_x[tid] += s_x[tid + s];
     }
     s *= 2;
   }
 
   // Write out once we're done reducing each block
-  if (tid == 0){
+  if (tid == 0)
+  {
     x_reduced[blockIdx.x] = s_x[0];
   }
 }
-    
-int main(int argc, char * argv[]){
+
+int main(int argc, char *argv[])
+{
 
   int N = 4194304;
-  if (argc > 1){
+  if (argc > 1)
+  {
     N = atoi(argv[1]);
   }
 
@@ -50,33 +57,35 @@ int main(int argc, char * argv[]){
 
   printf("N = %d, Block Size = %d, Number of Blocks = %d\n", N, BLOCK_SIZE, numBlocks);
 
-  float * x = new float[N];
-  float * x_reduced = new float[numBlocks];  
+  float *x = new float[N];
+  float *x_reduced = new float[numBlocks];
 
-  for (int i = 0; i < N; ++i){
+  for (int i = 0; i < N; ++i)
+  {
     x[i] = 1.f;
   }
 
   // Allocate memory and copy to the GPU
-  float * d_x;
-  float * d_x_reduced;  
+  float *d_x;
+  float *d_x_reduced;
   int size_x = N * sizeof(float);
   int size_x_reduced = numBlocks * sizeof(float);
-  cudaMalloc((void **) &d_x, size_x);
-  cudaMalloc((void **) &d_x_reduced, size_x_reduced);
-  
+  cudaMalloc((void **)&d_x, size_x);
+  cudaMalloc((void **)&d_x_reduced, size_x_reduced);
+
   // Copy memory over to the GPU
   cudaMemcpy(d_x, x, size_x, cudaMemcpyHostToDevice);
   cudaMemcpy(d_x_reduced, x_reduced, size_x_reduced, cudaMemcpyHostToDevice);
 
-  partial_reduction <<< numBlocks, BLOCK_SIZE >>> (N, d_x_reduced, d_x);
+  partial_reduction<<<numBlocks, BLOCK_SIZE>>>(N, d_x_reduced, d_x);
 
   // Copy memory back to the CPU
   cudaMemcpy(x_reduced, d_x_reduced, size_x_reduced, cudaMemcpyDeviceToHost);
 
   // Sum the elements of x_reduced
   float sum_x = 0.f;
-  for (int i = 0; i < numBlocks; ++i){
+  for (int i = 0; i < numBlocks; ++i)
+  {
     sum_x += x_reduced[i];
   }
 
@@ -93,8 +102,9 @@ int main(int argc, char * argv[]){
   cudaEventCreate(&stop);
   cudaEventRecord(start, 0);
 
-  for (int i = 0; i < num_trials; ++i){
-    partial_reduction <<< numBlocks, BLOCK_SIZE >>> (N, d_x_reduced, d_x);
+  for (int i = 0; i < num_trials; ++i)
+  {
+    partial_reduction<<<numBlocks, BLOCK_SIZE>>>(N, d_x_reduced, d_x);
   }
 
   cudaEventRecord(stop, 0);
@@ -107,7 +117,7 @@ int main(int argc, char * argv[]){
   float average_time = time / num_trials;
 
   printf("Time to run kernel on average: %6.6f ms.\n", average_time);
-  
+
 #endif
 
   return 0;
--- homework-4/reduction_v2.cu
+++ homework-4/reduction_v2.cu
@@ -4,41 +4,48 @@
 
 #define BLOCK_SIZE 128
 
-__global__ void partial_reduction(const int N, float *x_reduced, const float *x){
-  
+__global__ void partial_reduction(const int N, float *x_reduced, const float *x)
+{
+
   __shared__ float s_x[BLOCK_SIZE];
 
   const int i = blockDim.x * 2 * blockIdx.x + threadIdx.x;
   const int tid = threadIdx.x;
-  
+
   // Coalesced reads in
   s_x[tid] = 0.f;
-  if (i < N){ 
+  if (i < N)
+  {
     s_x[tid] = x[i] + x[i + BLOCK_SIZE];
   }
 
   // Number of "live" threads per block
   int alive = blockDim.x;
-  
-  while (alive > 1){
-    __syncthreads(); 
-    // Update the number of live threads    
+
+  while (alive > 1)
+  {
+    __syncthreads();
+    // Update the number of live threads
     alive /= 2;
-    if (tid < alive){
+    if (tid < alive)
+    {
       s_x[tid] += s_x[tid + alive];
     }
   }
 
   // Write out once we're done reducing each block
-  if (tid == 0){
+  if (tid == 0)
+  {
     x_reduced[blockIdx.x] = s_x[0];
   }
 }
-    
-int main(int argc, char * argv[]){
+
+int main(int argc, char *argv[])
+{
 
   int N = 4194304;
-  if (argc > 1){
+  if (argc > 1)
+  {
     N = atoi(argv[1]);
   }
 
@@ -47,38 +54,40 @@ int main(int argc, char * argv[]){
 
   printf("N = %d, Block Size = %d, Number of Blocks = %d\n", N, BLOCK_SIZE, numBlocks);
 
-  float * x = new float[N];
-  float * x_reduced = new float[numBlocks];  
+  float *x = new float[N];
+  float *x_reduced = new float[numBlocks];
 
-  for (int i = 0; i < N; ++i){
+  for (int i = 0; i < N; ++i)
+  {
     x[i] = 1.f;
   }
 
   // Allocate memory and copy to the GPU
-  float * d_x;
-  float * d_x_reduced;  
+  float *d_x;
+  float *d_x_reduced;
   int size_x = N * sizeof(float);
   int size_x_reduced = numBlocks * sizeof(float);
-  cudaMalloc((void **) &d_x, size_x);
-  cudaMalloc((void **) &d_x_reduced, size_x_reduced);
-  
+  cudaMalloc((void **)&d_x, size_x);
+  cudaMalloc((void **)&d_x_reduced, size_x_reduced);
+
   // Copy memory over to the GPU
   cudaMemcpy(d_x, x, size_x, cudaMemcpyHostToDevice);
   cudaMemcpy(d_x_reduced, x_reduced, size_x_reduced, cudaMemcpyHostToDevice);
 
-  partial_reduction <<< numBlocks / 2, BLOCK_SIZE >>> (N, d_x_reduced, d_x);
+  partial_reduction<<<numBlocks / 2, BLOCK_SIZE>>>(N, d_x_reduced, d_x);
 
   // Copy memory back to the CPU
   cudaMemcpy(x_reduced, d_x_reduced, size_x_reduced, cudaMemcpyDeviceToHost);
 
   // Sum the elements of x_reduced
   float sum_x = 0.f;
-  for (int i = 0; i < numBlocks; ++i){
+  for (int i = 0; i < numBlocks; ++i)
+  {
     sum_x += x_reduced[i];
   }
 
   // Known value of sum of elements
-  float target = (float) N;
+  float target = (float)N;
   printf("error = %f\n", fabs(sum_x - target));
 
 #if 1
@@ -90,8 +99,9 @@ int main(int argc, char * argv[]){
   cudaEventCreate(&stop);
   cudaEventRecord(start, 0);
 
-  for (int i = 0; i < num_trials; ++i){
-    partial_reduction <<< numBlocks / 2, BLOCK_SIZE >>> (N, d_x_reduced, d_x);
+  for (int i = 0; i < num_trials; ++i)
+  {
+    partial_reduction<<<numBlocks / 2, BLOCK_SIZE>>>(N, d_x_reduced, d_x);
   }
 
   cudaEventRecord(stop, 0);
@@ -104,7 +114,7 @@ int main(int argc, char * argv[]){
   float average_time = time / num_trials;
 
   printf("Time to run kernel on average: %6.6f ms.\n", average_time);
-  
+
 #endif
 
   return 0;
